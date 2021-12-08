ebd3ef0f1deca465e9095e4c53887c460f0645f7
cxqqsbf <1589567033@qq.com>
2021-12-07 21:43:05 -0800

    research question2

--- analyze.py
+++ analyze.py
@@ -1,10 +1,10 @@
 from pydriller.domain.commit import ModificationType, ModifiedFile
-
+# from pydriller import *
 ### To write better source file analysis: https://pydriller.readthedocs.io/en/latest/modifiedfile.html
 def analyze_src(modified_src_file: ModifiedFile):
     if modified_src_file.change_type == ModificationType.RENAME:
         return {
-            'value': 0.01,  # Rename is not valuable
+            'value': 0,  # Rename is not valuable
             'added_lines_py': 0,
             'deleted_lines_py': 0,
         }
--- pydriller10x.py
+++ pydriller10x.py
@@ -4,8 +4,12 @@ from datetime import date, datetime
 from analyze import *
 import numpy as np
 
-urls = ["https://github.com/ishepard/pydriller.git"]
-since = datetime(2020, 10, 8, 17, 0, 0)
+url1="https://github.com/tensorflow/tensorflow.git"
+url2="https://github.com/spring-projects/spring-boot.git"
+url3='https://github.com/NVIDIA/DeepLearningExamples'
+url4='D:/github_project/tensorflow/'
+urls = [url4]
+since = datetime(2019, 11, 10, 17, 0, 0)
 
 
 def evaluate_value_doc(modified_file: ModifiedFile):
@@ -16,11 +20,11 @@ def evaluate_value_doc(modified_file: ModifiedFile):
 commit_data = pd.DataFrame(columns=[
     ### Below are standard commit infomation
     'hash',
+    'author',
     'author_email',
     'author_date',
-    'insertions',
-    'deletions',
     'lines',
+    'dmm_score',
     ### Below are statistics from individual files
     'value',
     'added_lines_py',
@@ -29,6 +33,14 @@ commit_data = pd.DataFrame(columns=[
 
 for commit in Repository(path_to_repo=urls, since=since).traverse_commits():
     ### Each modified source file will produce a statistic
+    print("commit {}, date {}".format(
+        commit.hash, commit.author_date))
+    print('dmm_size{},dmm_complexity{},dmm_interfacing{}'.format(commit.dmm_unit_size,
+                commit.dmm_unit_complexity,
+                commit.dmm_unit_interfacing))
+
+
+
     modified_src_stat = pd.DataFrame(columns=[
         'value',
         'added_lines_py',
@@ -36,7 +48,7 @@ for commit in Repository(path_to_repo=urls, since=since).traverse_commits():
     ])
     doc_value = 0
     for modified_file in commit.modified_files:
-        if modified_file.filename.endswith(".py"):
+        if modified_file.filename.endswith((".py",".cc")):
             modified_src_stat = modified_src_stat.append(analyze_src(modified_file), ignore_index=True)
         if modified_file.filename.endswith((".md", "adoc")): # TODO: more doc file types
             doc_value += evaluate_value_doc(modified_file)
@@ -53,24 +65,33 @@ for commit in Repository(path_to_repo=urls, since=since).traverse_commits():
         'added_lines_py': 0,
         'deleted_lines_py': 0,
     }
+    if commit.dmm_unit_size!=None and commit.dmm_unit_complexity!=None and commit.dmm_unit_interfacing!=None:
+        dmm_score=commit.dmm_unit_size+commit.dmm_unit_complexity+ commit.dmm_unit_interfacing
+    else:
+        dmm_score=0
+    print('dmm_score')
+    print(dmm_score)
 
     commit_data = commit_data.append({
         'hash': commit.hash,
-        'author': commit.author.email,
+        'author':commit.author.name,
+        'author_email': commit.author.email,
         'author_date': commit.author_date,
-        'insertions': commit.insertions,
-        'deletions': commit.deletions,
         'lines': commit.lines,
         'value': stat['value'] + doc_value,
         'added_lines_py': stat['added_lines_py'],
         'deleted_lines_py': stat['deleted_lines_py'],
+        'dmm_score':dmm_score
     }, ignore_index=True)
+
+    commit_data=commit_data.drop(commit_data[commit_data['dmm_score']==-1].index)
 
 
 ######## Usage of generated commit data
-commit_data.to_csv('commit_data.csv')
+commit_data.to_csv('./commit_data.csv')
 commit_data.groupby('author').aggregate(
     commit_count = ('hash', 'size'),
     lines = ('lines', 'sum'),
-    value = ('value', 'sum')
+    value = ('value', 'sum'),
+    dmm_score=('dmm_score','sum')
 ).to_csv('data.csv')
